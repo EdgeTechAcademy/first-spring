f65686d8c8ead09f37f7dc0a0708ef8c9c807cfb
EdgeTechAcademy <gjames@ancoraeducation.com>
2016-11-21 09:25:37 -0600

    create

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello to my World!";
     }
 
     public static void main(String[] args) throws Exception {
